7628faf037f5b20fcaee4058d9a377ef171b4980
Luke Martin <lmartin.repo@gmail.com>
2026-09-07 04:47:04 +0000

    Complete homework 1

--- main.cpp
+++ main.cpp
@@ -8,4 +8,6 @@ int main()
 	//create a few starter objects
 	//add some values to test out your values methods
 	//print out all the values for each object to validate your code
+	std::cout << "Hello, World! Homework 1 completed." << std::endl;
+    return 0;
 }
